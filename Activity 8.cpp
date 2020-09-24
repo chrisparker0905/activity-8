c6a25d6e905ea20c19a29b9f44e28e1ebca597d2
chrisparker0905 <71851596+chrisparker0905@users.noreply.github.com>
2020-09-24 16:37:16 -0500

    Add files via upload
    need to figure out how to get if and if/else statements to be read

--- Activity 8.cpp
+++ Activity 8.cpp
@@ -17,10 +17,9 @@ execution results are also results of improper activities.Such
 violations may result in zero credit for the assignment, reduced
 credit for the assignment, or course failure.
 */
-/*A “magic date” is a date where the month times the day equals the
-two - digit year(when written mm / dd / yy - e.g., 2 / 8 / 16).Write a program
-to ask the user to input a month, dayand year.Then display whether
-it is a magic date or not.
+/*// Write a program to ask for a cartesian coordinate x,y. Your program should read in an x 
+// value and a y value and then inform the user whether it is the origin (0,0), on the y 
+// axis, eg (0,10), on the x axis
 */
 
 
@@ -29,9 +28,7 @@ it is a magic date or not.
 #include <conio.h>
 using namespace std;
 
-// Write a program to ask for a cartesian coordinate x,y. Your program should read in an x 
-// value and a y value and then inform the user whether it is the origin (0,0), on the y 
-// axis, eg (0,10), on the x axis
+
 
 
 int main()
@@ -47,31 +44,26 @@ int main()
     if (Q1 = (x, y))
         //print " coordinate in 1st quadrant"
     {
-        cout << "is first quadrant\n";
+        cout << "(x, y) is first quadrant\n";
     }
     else if (Q2 = (-x,y))
         //print coordinate in the 2nd quadrant
     {
-        cout << "is in the 2nd quadrant \n";
+        cout << "(-x, y) is in the 2nd quadrant \n";
 
 }
     else if (Q3 = (-x,-y))
     //print coordinate in the 3rd quadrant
     {
-    cout << " is in the 3rd quadrant\n";
+    cout << "(-x, -y) is in the 3rd quadrant\n";
 }
     else if (Q4 = (x, -y))
     //print coordinate in the 4th quadrant
     {
-    cout << " is in the 4th quadrant \n";
+    cout << "(x, -y) is in the 4th quadrant \n";
     }
     //have the information entered into the computer displayed back to the user as a magic number
 
     return 0;
 }
 
-/*Welcome to the magic date program, please enter a date
-Enter the digits of a month in mm form, Enter the digits of a month in dd form, Enter the digits of a month in yy form
-02 08 16
-its a magic date
-*/
